df73d5e61c988e3ba5a60f9766151d9b27d84874
Jan <131482887+jangmz@users.noreply.github.com>
2024-05-14 14:38:30 +0200

    working on authentication

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -15,10 +15,29 @@ export default function Login() {
         setUser({ ...user, password: e.target.value });
     }
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault();
         console.log("submit")
         console.log(user);
+
+        try {
+            const response = await fetch("http://webshopdemo.devweb.b-s.si/api/WebShopDemo/Account/Authenticate", {
+                mode: "cors",    
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            });
+
+            const data = await response.json();
+            console.log("Response data:");
+            console.log(data);
+            // handle data
+        } catch (error) {
+            console.error("Error: " + error);
+            // handle error
+        }
     }
     return (
         <div id="login">
@@ -26,13 +45,11 @@ export default function Login() {
                 <Input 
                     label="username" 
                     type="text" 
-                    value={user.username}
                     onChange={handleUsernameChange} 
                 />
                 <Input 
                     label="password" 
                     type="password" 
-                    value={user.password}
                     onChange={handlePasswordChange} 
                 />
                 <button>Authenticate</button>
