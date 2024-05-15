c869ae2b225943544d1e4af53ee2dd091c5bd372
Jan <131482887+jangmz@users.noreply.github.com>
2024-05-15 10:19:34 +0200

    refactored fetching function with useEffect

--- src/components/shop.jsx
+++ src/components/shop.jsx
@@ -1,14 +1,52 @@
 import TokenMessage from "./tokenMessage";
 import { TokenContext } from "../App";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import ItemCard from "./itemCard";
 
 export default function Shop() {
-    const {token, setToken} = useContext(TokenContext);
+    const { token } = useContext(TokenContext);
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(() => {
+        fetch("http://webshopdemo.devweb.b-s.si/api/public/WebShopDemo/pub/FLB/Item/?%24top=30", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-type": "application/json",
+            },
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("HTTP error: " + response.status);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            setItems(data);
+        })
+        .finally(() => setLoading(false));
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="message">
+                <p>Loading...</p>
+            </div>
+        )
+    }
 
     return (
         <>
             <h1>Shop</h1>
             <TokenMessage />
+            <div className="items">
+                {items.map(item => {
+                    <ItemCard item={item} />
+                })}
+            </div> 
         </>
     )
 }
