de665fd4db63c3b5ad3cfac95aa3a5b3c531fb45
Jan <131482887+jangmz@users.noreply.github.com>
2024-05-15 15:49:15 +0200

    added cart context

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,47 @@ import { Link, Outlet, useLocation } from 'react-router-dom'
 import TokenMessage from './components/tokenMessage';
 
 export const TokenContext = createContext();
+export const CartContext = createContext();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [cart, setCart] = useState([]);
   const location = useLocation();
   const isSubRoute = location.pathname !== "/";
 
   return (
     <TokenContext.Provider value={{ token, setToken }}>
-      <div className='navigation'>
-        <nav className='nav-menu'>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="shop">Shop</Link>
-            </li>
-            <li>
-              <Link to="cart">Cart</Link>
-            </li>
-            { token === null && <li>
-              <Link to="login">Authenticate</Link>
-            </li> }
-          </ul>
-        </nav>
-      </div>
-      <div className='content'>
-        {
-          !isSubRoute && (
-            <div className='home-page'>
-              <h1>Home</h1>
-              <TokenMessage />
-            </div>
-          )
-        }
-        <Outlet />
-      </div>
+      <CartContext.Provider value={{ cart, setCart }}>
+        <div className='navigation'>
+          <nav className='nav-menu'>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="shop">Shop</Link>
+              </li>
+              <li>
+                <Link to="cart">Cart ({cart.length})</Link>
+              </li>
+              { token === null && <li>
+                <Link to="login">Authenticate</Link>
+              </li> }
+            </ul>
+          </nav>
+        </div>
+        <div className='content'>
+          {
+            !isSubRoute && (
+              <div className='home-page'>
+                <h1>Home</h1>
+                <TokenMessage />
+              </div>
+            )
+          }
+          <Outlet />
+        </div>
+      </CartContext.Provider>
     </TokenContext.Provider>
   )
 }
